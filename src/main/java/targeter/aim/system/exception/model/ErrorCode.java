526b73deea214034a88eb9889cd6b71fdec37929
HuniHuns <i690367@gmail.com>
2025-11-01 17:37:54 +0900

    * feat: 예외 처리 및 오류 응답 DTO 추가
    - ErrorDto 클래스와 ValidationErrorResponse, ErrorResponse 내부 클래스를 추가하여 오류 응답 구조 정의
    - GlobalExceptionHandler 클래스 생성 및 다양한 예외 처리 로직 구현
    - ErrorCode 열거형 추가하여 공통 오류 코드 및 메시지 관리
    - RestException 클래스 추가하여 사용자 정의 예외 처리 가능하도록 설정
    - build.gradle 파일에 Lombok 의존성 추가
    
    * feat: 웹 애플리케이션 서블릿 설정 추가
    
    - WebAppServletConfig 클래스를 생성하여 정적 리소스 핸들링 및 Jackson 커스터마이저 설정 구현
    - LocalDate 및 LocalDateTime에 대한 직렬화 및 역직렬화 로직 추가
    
    * feat: 감사 엔티티 클래스 추가
    
    - TimeStampedEntity 및 UserStampedEntity 클래스를 추가하여 생성 및 수정 날짜를 자동으로 관리하는 감사 기능 구현
    
    * feat: 애플리케이션 설정 파일 추가 및 기존 파일 삭제
    
    - 로컬 및 원격 환경을 위한 application-local.yml 및 application-remote.yml 파일 추가
    - 기존 application.properties 파일 삭제
    - application.yml 파일 추가하여 프로파일 및 서버 설정 포함

---
+++ src/main/java/targeter/aim/system/exception/model/ErrorCode.java
@@ -0,0 +1,22 @@
+package targeter.aim.system.exception.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    // Global
+    GLOBAL_BAD_REQUEST(400, "올바르지 않은 요청입니다."),
+    GLOBAL_NOT_FOUND(404, "요청한 사항을 찾을 수 없습니다."),
+    GLOBAL_ALREADY_EXIST(400, "요청의 대상이 이미 존재합니다."),
+    GLOBAL_METHOD_NOT_ALLOWED(405, "허용되지 않는 Method 입니다."),
+    GLOBAL_INVALID_PARAMETER(400, "올바르지 않은 파라미터입니다."),
+    INVALID_INPUT_VALUE(400, "유효하지 않은 입력 값입니다."),
+
+    // Other
+    INTERNAL_SERVER_ERROR(500, "오류가 발생했습니다."), ;
+
+    private final int statusCode;
+    private final String message;
+}
