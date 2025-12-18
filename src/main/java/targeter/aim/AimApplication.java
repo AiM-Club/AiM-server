c1c46843c5dcc6441882230f303804bc144aa1a5
limdabeen <ldb74777@naver.com>
2025-12-18 14:13:45 +0900

    refact: 파일 도메인 Handler 제거 및 File DTO 컨벤션 적용

--- src/main/java/targeter/aim/AimApplication.java
+++ src/main/java/targeter/aim/AimApplication.java
@@ -2,20 +2,11 @@ package targeter.aim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(basePackages = {
-        // AttachedFile 및 TestImageFile 엔티티 경로
-        "targeter.aim.domain.user.file.entity",
-        // User 엔티티 경로
-        "targeter.aim.domain.user.entity"
-        // 프로젝트에 존재하는 다른 모든 엔티티 패키지 경로도 여기에 추가해야 합니다.
-})
 @SpringBootApplication
 public class AimApplication {
 
     public static void main(String[] args) {
-        // 💡 서버가 시작되자마자 종료되는 문제를 해결하기 위해 이 코드가 필수입니다.
         SpringApplication.run(AimApplication.class, args);
     }
 }
--- src/main/java/targeter/aim/domain/user/file/controller/FileController.java
+++ src/main/java/targeter/aim/domain/user/file/controller/FileController.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import targeter.aim.domain.user.file.entity.AttachedFile;
 import targeter.aim.domain.user.file.service.FileService;
 import targeter.aim.system.security.annotation.NoJwtAuth;
 
@@ -18,21 +21,31 @@ public class FileController {
 
     private final FileService fileService;
 
-
     @NoJwtAuth("이미지 조회는 공개 리소스로 인증이 필요하지 않음")
-    @GetMapping("/images/{file_uuId}")
+    @GetMapping("/images/{file_uuid}")
     @Operation(summary = "이미지 조회", description = "특정 아이디로 저장된 이미지를 조회합니다.")
     @ApiResponse(responseCode = "200", description = "이미지 조회 성공")
-    public ResponseEntity<Resource> viewImage(@PathVariable("file_uuId") String fileUuid) {
-        return fileService.getImage(fileUuid);
+    public ResponseEntity<Resource> viewImage(@PathVariable("file_uuid") String fileUuid) {
+        FileService.FileResource fr = fileService.getImage(fileUuid);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(fr.resource());
     }
 
-
     @NoJwtAuth("파일 다운로드는 공개 리소스로 인증이 필요하지 않음")
-    @GetMapping("/downloads/{file_uuId}")
+    @GetMapping("/downloads/{file_uuid}")
     @Operation(summary = "파일 다운로드", description = "특정 아이디로 저장된 파일을 다운로드합니다.")
     @ApiResponse(responseCode = "200", description = "파일 다운로드 성공")
-    public ResponseEntity<Resource> downloadFile(@PathVariable("file_uuId") String fileUuid) {
-        return fileService.downloadFile(fileUuid);
+    public ResponseEntity<Resource> downloadFile(@PathVariable("file_uuid") String fileUuid) {
+        FileService.FileResource fr = fileService.downloadFile(fileUuid);
+        AttachedFile file = fr.attachedFile();
+
+        String contentDisposition = "attachment; filename=\"" + file.getFileName() + "\"";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(fr.resource());
     }
 }
