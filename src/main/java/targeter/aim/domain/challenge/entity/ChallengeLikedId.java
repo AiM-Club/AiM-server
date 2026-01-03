eeebc24847d663fd3530ad804dcab73e7005b91d
ksy132336 <ksygiru@gmail.com>
2026-01-04 03:53:16 +0900

    feat: ChallengeLiked 엔티티 및 복합키 추가

---
+++ src/main/java/targeter/aim/domain/challenge/entity/ChallengeLikedId.java
@@ -0,0 +1,20 @@
+package targeter.aim.domain.challenge.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ChallengeLikedId implements Serializable {
+
+    private Long userId;
+    private Long challengeId;
+}
