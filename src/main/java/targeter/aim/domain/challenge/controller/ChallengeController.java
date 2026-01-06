0dfca5a9855c323db4b5d4f1f88c736b8e7c1026
HuniHuns <i690367@gmail.com>
2026-01-06 17:41:24 +0900

    feat: 챌린지 생성 컨트롤러 구현

---
+++ src/main/java/targeter/aim/domain/challenge/controller/ChallengeController.java
@@ -0,0 +1,26 @@
+package targeter.aim.domain.challenge.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import targeter.aim.domain.challenge.dto.ChallengeDto;
+import targeter.aim.domain.challenge.service.ChallengeService;
+import targeter.aim.system.security.model.UserDetails;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/challenge")
+public class ChallengeController {
+    private ChallengeService challengeService;
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ChallengeDto.ChallengeDetailsResponse createChallenge(
+            @RequestBody ChallengeDto.ChallengeCreateRequest request,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        return challengeService.createChallenge(userDetails, request);
+    }
+}
