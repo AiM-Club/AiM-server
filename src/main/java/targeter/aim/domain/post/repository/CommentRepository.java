6f4c29febdff5f177f50d6b579a58b4d455e57c2
ksy132336 <ksygiru@gmail.com>
2026-01-22 02:06:54 +0900

    feat: JPARepository 생성

---
+++ src/main/java/targeter/aim/domain/post/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package targeter.aim.domain.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import targeter.aim.domain.post.entity.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
